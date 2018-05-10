5e7ed2ee8eb604ef4749b93e9625a37f66f8ed85
felipe <felipe904059>
2018-05-10 20:20:13 -0300

    Versión uno, código sin refactorizar

---
+++ Cliente.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;            //   Paso 1
+using System.Net.Sockets;    //   Paso 1
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace Cliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conectar();
+        }
+        public static void Conectar()
+        {
+            Socket miPrimerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint miDireccion = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 2048);
+
+            string texto;
+            byte[] textoEnviar;
+            string respuesta = "";
+
+
+
+            try
+            {
+                miPrimerSocket.Connect(miDireccion);
+                Console.WriteLine("Conectado con exito");
+                byte[] ByRec = new byte[255];
+                do
+                {
+                    for (int i = 0; i < 2; i++){
+                        if(i == 0){
+                            
+                            Console.WriteLine("Ingrese su usuario: ");
+                            texto = Console.ReadLine(); //leo el texto ingresado 
+                            textoEnviar = Encoding.Default.GetBytes(texto); //pasamos el texto a array de bytes
+                            miPrimerSocket.Send(textoEnviar, 0, textoEnviar.Length, 0); // y lo enviamos
+                            Console.WriteLine("Usuario enviado exitosamente");
+                            //int a = miPrimerSocket.Receive(ByRec, 0, ByRec.Length, 0);
+                            //Array.Resize(ref ByRec, a);
+                            //Console.WriteLine("Servidor responde: " + Encoding.Default.GetString(ByRec)); //mostramos lo recibido
+
+                        } else if (i == 1) {
+                            
+                            Console.WriteLine("Ingrese su clave: ");
+                            texto = Console.ReadLine(); //leo el texto ingresado 
+                            textoEnviar = Encoding.Default.GetBytes(texto); //pasamos el texto a array de bytes
+                            miPrimerSocket.Send(textoEnviar, 0, textoEnviar.Length, 0); // y lo enviamos
+                            Console.WriteLine("Clave enviada exitosamente");
+                            int a = miPrimerSocket.Receive(ByRec, 0, ByRec.Length, 0);
+                            Array.Resize(ref ByRec, a);
+                            respuesta = Encoding.Default.GetString(ByRec);
+                            Console.WriteLine("Servidor responde: " + Encoding.Default.GetString(ByRec)); //mostramos lo recibido
+
+                        }
+
+                    }
+                     if(respuesta == "true"){
+                        do{
+                            Inicio:
+                            //Muestra el menú de ociones
+                            Menu.MenuUsuario._menuUsuario();
+
+                            //Crea el arreglo de bytes a enviar.
+                            byte[] ByRec2 = new byte[255];
+                            byte[] textoEnvia;
+                            //Le pide al usuario ingresar su opción, entero.
+                            Console.WriteLine("Ingrese su opción: ");
+                            texto = Console.ReadLine(); //leo el texto ingresado 
+                            textoEnvia = Encoding.Default.GetBytes(texto); //pasamos el texto a array de bytes
+                            miPrimerSocket.Send(textoEnvia, 0, textoEnvia.Length, 0); // le envío la opción al cliente.
+                            Console.WriteLine("Opcion enviada"); //Confirmamos al cliente que la opción fue enviada.
+
+                            switch (int.Parse(texto))
+                            {
+                                case 1:
+                                    Console.WriteLine("Ingrese el nombre del usuario: "); //Pide ingresar el nombre del usuario
+                                    texto = Console.ReadLine(); //leo el texto ingresado 
+                                    textoEnviar = Encoding.Default.GetBytes(texto); //pasamos el texto a array de bytes
+                                    miPrimerSocket.Send(textoEnviar, 0, textoEnviar.Length, 0); // le envío la opción al cliente.
+                                    Console.WriteLine("Nombre guardado"); //Confirmamos al cliente que la opción fue enviada.
+
+                                    int ab = miPrimerSocket.Receive(ByRec2, 0, ByRec2.Length, 0); // se recibe el texto desde el servidor.
+                                    Array.Resize(ref ByRec2, ab);
+                                    Console.WriteLine("Servidor responde: " + Encoding.Default.GetString(ByRec2)); //mostramos lo recibido
+                                    Console.WriteLine("");
+                                    break;
+
+                                case 2:
+                                    Console.WriteLine("Elegí la lista de usuarios");
+
+                                    byte[] malditaLista = new byte[255];
+                                    int ab64 = miPrimerSocket.Receive(malditaLista, 0, malditaLista.Length, 0);
+                                    Array.Resize(ref malditaLista, ab64);
+                                    Console.WriteLine("Servidor envía: \n" + Encoding.Default.GetString(malditaLista));
+
+
+                                    break;
+                                case 3:
+                                    byte[] B2 = new byte[255];
+                                    int ab66 = miPrimerSocket.Receive(B2, 0, B2.Length, 0);
+                                    Array.Resize(ref B2, ab66);
+                                    Console.WriteLine("Servidor envía: " + Encoding.Default.GetString(B2));
+
+                                    if (Encoding.Default.GetString(B2) == "Lista vacía")
+                                    {
+                                        goto Inicio;
+                                    }else{
+                                        texto = Console.ReadLine(); //leo el texto ingresado 
+                                        textoEnviar = Encoding.Default.GetBytes(texto); //pasamos el texto a array de bytes
+                                        miPrimerSocket.Send(textoEnviar, 0, textoEnviar.Length, 0);
+
+                                        int ab665 = miPrimerSocket.Receive(B2, 0, B2.Length, 0);
+                                        Array.Resize(ref B2, ab665);
+                                        Console.WriteLine("Servidor envía: " + Encoding.Default.GetString(B2));
+                                    }
+                                    break;
+                                default:
+                                    byte[] B23 = new byte[255];
+                                    int ab662 = miPrimerSocket.Receive(B23, 0, B23.Length, 0);
+                                    Array.Resize(ref B23, ab662);
+                                    Console.WriteLine("Servidor envía: " + Encoding.Default.GetString(B23));
+                                    goto Adios;
+                                    break;
+                            }
+
+
+                        }while(true);
+                    }else{
+                        Console.WriteLine("Lo lamento, usted no tiene permisos para hacer eco");
+                    }
+
+                } while (false);
+
+                miPrimerSocket.Close();
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Error: {0}", error.ToString());
+            }
+            Adios:
+            Console.WriteLine("Presione cualquier tecla para terminar");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ServidorEcho.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;               
+using System.Net.Sockets;
+using ServidorEcho.Modelos;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+ 
+namespace Servidor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conectar();
+        }
+
+        public static void Conectar()
+        {
+            Socket miPrimerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint miDireccion = new IPEndPoint(IPAddress.Any, 2048);
+
+            try
+            {
+                miPrimerSocket.Bind(miDireccion);
+
+                List<Usuario> _listaUsuarios = new List<Usuario>();
+
+                Escuchando:
+                miPrimerSocket.Listen(1);
+                Console.WriteLine("Esperando la conexión del cliente...");
+                Socket Escuchar = miPrimerSocket.Accept();
+                Console.WriteLine("Conectado con exito");
+                int i = 0;
+                byte[] textoEnviars;
+                string[] credenciales = new string[2];
+                int opcionCliente = 0;
+
+
+                //_listaUsuarios.Add(new Usuario("Felipe"));
+                //_listaUsuarios.Add(new Usuario("Antonio"));
+
+                do
+                {
+                    byte[] ByRec = new byte[255];
+                    int a = Escuchar.Receive(ByRec, 0, ByRec.Length, 0);
+                    Array.Resize(ref ByRec, a);
+
+                    if(i == 0){
+                        credenciales[0] = Encoding.Default.GetString(ByRec);
+                    }
+                    if(i == 1){
+                        credenciales[1] = Reverse(Encoding.Default.GetString(ByRec));
+
+                        Console.WriteLine("usuario: "+credenciales[0]);
+                        Console.WriteLine("clave: "+credenciales[1]);
+
+
+                        if(credenciales[0] == credenciales[1]){
+
+                            textoEnviars = Encoding.Default.GetBytes("true"); //pasamos el texto a array de bytes
+                            Escuchar.Send(textoEnviars, 0, textoEnviars.Length, 0); // y lo enviamos
+
+                            Console.WriteLine("Las credenciales coinciden.");
+
+                            Console.WriteLine("Comienza el eco! :D");
+                            do{
+                                Inicio:
+                                byte[] ByRec2 = new byte[255];
+                                int ab = Escuchar.Receive(ByRec2, 0, ByRec2.Length, 0);
+                                Array.Resize(ref ByRec2, ab);
+                                Console.WriteLine("Cliente dice: " + Encoding.Default.GetString(ByRec2));
+
+                                try{
+                                    opcionCliente = Int32.Parse(Encoding.Default.GetString(ByRec2));
+                                }catch(Exception e){
+                                    Console.WriteLine(e);
+                                    goto Inicio;
+                                }
+
+                                switch (opcionCliente)
+                                {
+                                    case 1:
+                                        byte[] textoEnviar = Encoding.Default.GetBytes("Eligió crear usuario");
+                                        Console.WriteLine("Servidor envía: " + Encoding.Default.GetString(textoEnviar));
+                                        byte[] ByRec3 = new byte[255];
+                                        int ab2 = Escuchar.Receive(ByRec3, 0, ByRec3.Length, 0);
+                                        Array.Resize(ref ByRec3, ab2);
+
+                                        Console.WriteLine("Cliente dice: " + Encoding.Default.GetString(ByRec3));
+
+                                        string nombreuser = Encoding.Default.GetString(ByRec3);
+
+                                        _listaUsuarios.Add(new Usuario(nombreuser));
+
+                                        textoEnviar = Encoding.Default.GetBytes(Encoding.Default.GetString(ByRec3) + " (Nombre guardado).");
+
+                                        Escuchar.Send(textoEnviar, 0, textoEnviar.Length, 0); // y lo enviamos
+
+                                        textoEnviar = null;
+                                        opcionCliente = 0;
+                                        ByRec3 = null;
+                                        break;
+
+                                    case 2:
+                                        Console.WriteLine("El cliente quiere la lista.");
+
+                                        if(_listaUsuarios.Count == 0){
+                                            textoEnviar = Encoding.Default.GetBytes("La lista está vacía."); //pasamos el texto a array de bytes
+                                            Escuchar.Send(textoEnviar, 0, textoEnviar.Length, 0);
+                                        }else{
+                                            string lista = retornaLista(_listaUsuarios);
+
+                                            Console.WriteLine(lista);
+                                            //Enviar la lista
+                                            textoEnviar = Encoding.Default.GetBytes(lista); //pasamos el texto a array de bytes
+                                            Escuchar.Send(textoEnviar, 0, textoEnviar.Length, 0);
+                                        }
+
+
+                                        break;
+
+                                    case 3:
+                                        if(_listaUsuarios.Count == 0){
+                                            textoEnviars = Encoding.Default.GetBytes("Lista vacía"); //pasamos el texto a array de bytes
+                                            Escuchar.Send(textoEnviars, 0, textoEnviars.Length, 0); // y lo enviamos
+                                        }else{
+                                            textoEnviars = Encoding.Default.GetBytes("Ingrese ID del usuario a eliminar:"); //pasamos el texto a array de bytes
+                                            Escuchar.Send(textoEnviars, 0, textoEnviars.Length, 0); // y lo enviamos
+
+                                            //Escuchamos la id que quiere eliminar
+                                            byte[] ByRec55 = new byte[255];
+                                            int ab55 = Escuchar.Receive(ByRec55, 0, ByRec55.Length, 0);
+                                            Array.Resize(ref ByRec55, ab55);
+
+                                            int id = int.Parse(Encoding.Default.GetString(ByRec55));
+
+                                            try
+                                            {
+                                                _listaUsuarios.RemoveAt(id);
+                                            }
+                                            catch (Exception)
+                                            {
+                                                Console.WriteLine("Hubo un error, vuelve al inicio.");
+                                                goto Inicio;
+                                            }
+
+
+                                            //Confirmamos que se eliminó.
+                                            textoEnviars = Encoding.Default.GetBytes("Usuario eliminado."); //pasamos el texto a array de bytes
+                                            Escuchar.Send(textoEnviars, 0, textoEnviars.Length, 0); // y lo enviamos
+
+                                            foreach (Usuario v in _listaUsuarios)
+                                            {
+                                                Console.WriteLine(v.Nombre);
+                                            }
+
+                                            goto Inicio;
+                                        }
+
+                                        break;
+                                    default:
+                                        textoEnviars = Encoding.Default.GetBytes("Adios"); //pasamos el texto a array de bytes
+                                        Escuchar.Send(textoEnviars, 0, textoEnviars.Length, 0); // y lo enviamos
+                                        Escuchar.Close();
+                                        goto Escuchando;
+                                        break;
+                                }
+
+
+
+
+                            }while(true);
+                        }else{
+                            textoEnviars = Encoding.Default.GetBytes("Las credenciales No coinciden."); //pasamos el texto a array de bytes
+                            Escuchar.Send(textoEnviars, 0, textoEnviars.Length, 0); // y lo enviamos
+                            Escuchar.Close();
+                            goto Escuchando;
+                        }
+
+                    }
+
+
+                    i = i + 1;
+
+
+                } while (true);
+
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Error: {0}", error.ToString());
+            }
+            Console.WriteLine("Presione cualquier tecla para terminar");
+            Console.ReadLine();
+
+        }
+
+        public static string Reverse(string text)
+        {
+            if (text == null) return null;
+
+            // this was posted by petebob as well 
+            char[] array = text.ToCharArray();
+            Array.Reverse(array);
+            return new String(array);
+        }
+
+        public static string retornaLista(List<Usuario> lista){
+
+            string cadena = "";
+            int contar = -1;
+
+            foreach(Usuario u in lista){
+                contar++;
+                cadena = contar + "). " + u.Nombre + "\n" + cadena;
+            }
+
+            return cadena;
+        }
+    }
+}
